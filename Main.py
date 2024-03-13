61b818acb5028e3fe9b27ca04799b3e7f59ad66e
coleaskey32 <105457021+coleaskey32@users.noreply.github.com>
2024-03-13 10:16:18 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,8 @@ def imShow(path):
   image = cv2.imread(path)
   height, width = image.shape[:2]
   resized_image = cv2.resize(image,(3*width, 3*height), interpolation = cv2.INTER_CUBIC)
-
+  
+  print(resized_image)
   fig = plt.gcf()
   fig.set_size_inches(18, 10)
   plt.axis("off")
@@ -23,9 +24,11 @@ darknet_lib = ctypes.CDLL('/home/colea/darknet/libdarknet.so')
 
 ################## Main Program ##################
 
-print("HELLLLOOOOO")
-# Command to execute
-command = ['/home/colea/darknet/darknet', 'detector', 'test', '/home/colea/darknet/data/obj.data', '/home/colea/darknet/cfg/yolov4-tiny-custom.cfg', '/home/colea/darknet/cfg/yolov4.weights', '-ext_output', '/home/colea/darknet/test_images/SoccerBall.jpg']
+# Capturing from webcam
+cap = cv2.VideoCapture(0)
+
+# Command to executeclear
+command = ['/home/colea/darknet/darknet', 'detector', 'test', '/home/colea/darknet/data/obj.data', '/home/colea/darknet/cfg/yolov4-tiny-custom.cfg', '/home/colea/darknet/cfg/yolov4.weights', '-ext_output', '/home/colea/S1.jpg']
 
 # Execute the command
 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -43,4 +46,4 @@ else:
     print("Command executed successfully")
     
     
-#imShow('predictions.jpg')
+imShow('predictions.jpg')
